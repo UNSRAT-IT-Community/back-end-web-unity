b8eb324f6e8e22bd62ec3f1baea6e332bab2291f
albert19_ <albertnanda1902@gmail.com>
2024-07-04 21:58:00 +0800

    feat(firebase): create trait for upload image to firebase storage

---
+++ app/Traits/FirebaseStorageTrait.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Traits;
+
+use Kreait\Firebase\Factory;
+use Kreait\Firebase\Storage;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+trait FirebaseStorageTrait
+{
+    protected $firebaseStorage;
+
+    public function initializeFirebaseStorage()
+    {
+        $this->firebaseStorage = (new Factory)
+            ->withServiceAccount(storage_path('app/firebase/firebase_credentials.json'))
+            ->createStorage();
+    }
+
+    public function uploadImageToFirebase(UploadedFile $file, $folder)
+    {
+        if (!$file->isValid()) {
+            throw new \Exception('File is not valid');
+        }
+
+        $extension = $file->getClientOriginalExtension();
+        $fileName = uniqid() . '.' . $extension;
+        $fileContent = file_get_contents($file->getRealPath());
+        $firebasePath = "{$folder}/{$fileName}";
+
+        $this->firebaseStorage->getBucket()->upload($fileContent, [
+            'name' => $firebasePath
+        ]);
+
+        $expiresAt = new \DateTime('+1 hour'); 
+        $signedUrl = $this->firebaseStorage->getBucket()->object($firebasePath)->signedUrl($expiresAt);
+
+        return $signedUrl;
+    }
+}
