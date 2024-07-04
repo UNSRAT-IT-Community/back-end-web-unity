720535084abef9fae29b6e50a895f292939c5373
albert19_ <albertnanda1902@gmail.com>
2024-07-04 21:56:57 +0800

    chore: change user password seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -23,15 +23,15 @@ class UserSeeder extends Seeder
         $divisionIds = DB::table('divisions')->pluck('id');
 
         $users = [
-            ['name' => 'John Doe', 'nim' => '210211060001', 'email' => 'john@example.com', 'profile_picture' => 'https://example.com/path/to/image1.jpg', 'password' => bcrypt('password123'), 'is_accepted' => 'accepted'],
-            ['name' => 'Jane Smith', 'nim' => '210211060002', 'email' => 'jane@example.com', 'profile_picture' => 'https://example.com/path/to/image2.jpg', 'password' => bcrypt('password456'), 'is_accepted' => 'accepted'],
-            ['name' => 'Alice Johnson', 'nim' => '210211060003', 'email' => 'alice@example.com', 'profile_picture' => 'https://example.com/path/to/image3.jpg', 'password' => bcrypt('password789'), 'is_accepted' => 'accepted'],
-            ['name' => 'Bob Brown', 'nim' => '210211060004', 'email' => 'bob@example.com', 'profile_picture' => 'https://example.com/path/to/image4.jpg', 'password' => bcrypt('password101112'), 'is_accepted' => 'pending'],
-            ['name' => 'Charlie Davis', 'nim' => '210211060005', 'email' => 'charlie@example.com', 'profile_picture' => 'https://example.com/path/to/image5.jpg', 'password' => bcrypt('password131415'), 'is_accepted' => 'pending'],
-            ['name' => 'Daisy Evans', 'nim' => '210211060006', 'email' => 'daisy@example.com', 'profile_picture' => 'https://example.com/path/to/image6.jpg', 'password' => bcrypt('password161718'), 'is_accepted' => 'pending'],
-            ['name' => 'Ella Fitzgerald', 'nim' => '210211060007', 'email' => 'ella@example.com', 'profile_picture' => 'https://example.com/path/to/image7.jpg', 'password' => bcrypt('password192021'), 'is_accepted' => 'denied'],
-            ['name' => 'George Harris', 'nim' => '210211060008', 'email' => 'george@example.com', 'profile_picture' => 'https://example.com/path/to/image8.jpg', 'password' => bcrypt('password222324'), 'is_accepted' => 'denied'],
-            ['name' => 'Ivy Johnson', 'nim' => '210211060009', 'email' => 'ivy@example.com', 'profile_picture' => 'https://example.com/path/to/image9.jpg', 'password' => bcrypt('password252627'), 'is_accepted' => 'denied']
+            ['name' => 'John Doe', 'nim' => '210211060001', 'email' => 'john@example.com', 'profile_picture' => 'https://example.com/path/to/image1.jpg', 'password' => bcrypt('password'), 'is_accepted' => 'accepted'],
+            ['name' => 'Jane Smith', 'nim' => '210211060002', 'email' => 'jane@example.com', 'profile_picture' => 'https://example.com/path/to/image2.jpg', 'password' => bcrypt('password'), 'is_accepted' => 'accepted'],
+            ['name' => 'Alice Johnson', 'nim' => '210211060003', 'email' => 'alice@example.com', 'profile_picture' => 'https://example.com/path/to/image3.jpg', 'password' => bcrypt('password'), 'is_accepted' => 'accepted'],
+            ['name' => 'Bob Brown', 'nim' => '210211060004', 'email' => 'bob@example.com', 'profile_picture' => 'https://example.com/path/to/image4.jpg', 'password' => bcrypt('password'), 'is_accepted' => 'pending'],
+            ['name' => 'Charlie Davis', 'nim' => '210211060005', 'email' => 'charlie@example.com', 'profile_picture' => 'https://example.com/path/to/image5.jpg', 'password' => bcrypt('password'), 'is_accepted' => 'pending'],
+            ['name' => 'Daisy Evans', 'nim' => '210211060006', 'email' => 'daisy@example.com', 'profile_picture' => 'https://example.com/path/to/image6.jpg', 'password' => bcrypt('password'), 'is_accepted' => 'pending'],
+            ['name' => 'Ella Fitzgerald', 'nim' => '210211060007', 'email' => 'ella@example.com', 'profile_picture' => 'https://example.com/path/to/image7.jpg', 'password' => bcrypt('password'), 'is_accepted' => 'denied'],
+            ['name' => 'George Harris', 'nim' => '210211060008', 'email' => 'george@example.com', 'profile_picture' => 'https://example.com/path/to/image8.jpg', 'password' => bcrypt('password'), 'is_accepted' => 'denied'],
+            ['name' => 'Ivy Johnson', 'nim' => '210211060009', 'email' => 'ivy@example.com', 'profile_picture' => 'https://example.com/path/to/image9.jpg', 'password' => bcrypt('password'), 'is_accepted' => 'denied']
         ];
 
         foreach ($users as $user) {
