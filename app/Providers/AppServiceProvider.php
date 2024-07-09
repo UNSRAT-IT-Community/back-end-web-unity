125688d821319f310ba50a8b03ca9bdf25006cd6
reyimanuel <miraclesumajow32@gmail.com>
2024-07-09 14:43:36 +0800

    Binding repository & interfaces

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,11 +2,13 @@
 
 namespace App\Providers;
 
-use App\Http\Interfaces\UpcomingEventRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
 use App\Http\Interfaces\UserRepositoryInterface;
+use App\Http\Interfaces\UpcomingEventRepositoryInterface;
+use App\Http\Interfaces\AnnouncementRepositoryInterface;
 use App\Http\Repositories\UserRepository;
 use App\Http\Repositories\UpcomingEventRepository; 
+use App\Http\Repositories\AnnouncementRepository;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -17,6 +19,7 @@ class AppServiceProvider extends ServiceProvider
     {
         $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
         $this->app->bind(UpcomingEventRepositoryInterface::class, UpcomingEventRepository::class);
+        $this->app->bind(AnnouncementRepositoryInterface::class, AnnouncementRepository::class);
     }
 
     /**
